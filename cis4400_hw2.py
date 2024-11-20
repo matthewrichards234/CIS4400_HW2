5df8dc49cf0b1bf1b1a928efc8e9b2a66f51a8e3
Matthew Richards <matthewrichards234@gmail.com>
2024-11-20 14:31:44 -0500

    Update cis4400_hw2.py

--- cis4400_hw2.py
+++ cis4400_hw2.py
@@ -1,2 +1,13 @@
 # LINK: https://data.cityofnewyork.us/City-Government/Film-Permits/tg4x-b46p/about_data
+
+# Data Sourcing
 from bs4 import BeautifulSoup
+
+# Storage
+
+# Deliverables
+
+# Transformation
+
+# Serving Data
+
